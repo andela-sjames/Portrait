5db13c1adf0b4566acc69b61f11a79e8b31a9a3c
andela-sjames <samuel.james@andela.com>
2019-05-17 04:33:13 +0100

    Dockerize app

--- portrait/portrait/settings.py
+++ portrait/portrait/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'portrait.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PORT': os.getenv('POSTGRESDB_PORT'),
+        'HOST': os.getenv('POSTGRESDB_HOST'),
     }
 }
 
--- portrait/webapp/admin.py
+++ portrait/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+from webapp.models import SocialProfile
+
 # Register your models here.
+admin.site.register(SocialProfile)
